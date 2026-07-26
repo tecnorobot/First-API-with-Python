30795cacbadf5966520d7f8af921c4963c09de9d
tecnorobot <abdulrahmanalbaiti83@gmail.com>
2026-07-26 18:36:27 +0300

    Stage 2: read endpoints with 404

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import *
 from fastapi.responses import *
+from pydantic import BaseModel
 
 app=FastAPI()
+
+
+class Task(BaseModel):
+    title : str
+    completed : bool=False
+
+task_DB={
+    1:{"title ":"Set up Git","completed":False},
+    2:{"title ":"Watch a vedio","completed":False},
+    3:{"title ":"push the project","completed":True}
+}
+
 
 @app.get("/")
 def getTasks() :
@@ -15,5 +28,16 @@ def getTasks() :
 def healthCheck():
     return {"status":"ok"}
 
+
+@app.get("/tasks")
+def getAllTasks():
+    return task_DB
    
+
+@app.get("/tasks/{task_id}")
+def getTaskById(task_id :int):
+    if task_id not in task_DB:
+      raise  HTTPException(status_code=404,detail="Task not found")
     
+
+    return task_DB[task_id]
