266c9ff01e3499f2e64eb34181e48ac717139665
tecnorobot <abdulrahmanalbaiti83@gmail.com>
2026-07-26 19:36:28 +0300

    Stage 3: create with validation

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import *
 from fastapi.responses import *
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app=FastAPI()
 
 
 class Task(BaseModel):
-    title : str
+    title : str 
     completed : bool=False
 
 task_DB={
@@ -41,3 +41,20 @@ def getTaskById(task_id :int):
     
 
     return task_DB[task_id]
+
+
+@app.post("/tasks",status_code=status.HTTP_201_CREATED)
+def creatTask(task:Task):
+    if not task.title:
+        raise HTTPException(status_code=400,detail="Title is empty") 
+
+    #Here I am going to check if database is empty to start with 1 or from new position
+    new_id=max(task_DB.keys())+1 if task_DB else 1
+
+    task_DB[new_id]=task.model_dump()
+
+    return {
+        "message":"Task successfully created",
+        "id":new_id,
+        "task":task_DB[new_id]
+        }
