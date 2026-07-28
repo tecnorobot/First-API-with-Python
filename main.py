b63823bcfb3bd5b3a09410f1cc5161b22401bf1a
tecnorobot <abdulrahmanalbaiti83@gmail.com>
2026-07-29 01:02:13 +0300

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ class Task(BaseModel):
     completed : bool=False
 
 task_DB={
-    1:{"title ":"Set up Git","completed":False},
-    2:{"title ":"Watch a vedio","completed":False},
-    3:{"title ":"push the project","completed":True}
+    1:{"title":"Set up Git","completed":False},
+    2:{"title":"Watch a vedio","completed":False},
+    3:{"title":"push the project","completed":True}
 }
 
 
@@ -58,3 +58,36 @@ def creatTask(task:Task):
         "id":new_id,
         "task":task_DB[new_id]
         }
+
+
+
+@app.put("/tasks/{task_id}")
+def updateTask(task_id: int,task_update: Task):
+
+    if task_id not in task_DB :
+        raise HTTPException(
+            status_code=404,
+            detail="Task not found"
+        )
+    newTask=task_update.model_dump(exclude_unset=True)
+
+    if not newTask :
+        raise HTTPException(
+            status_code=400,
+            detail="Request body cannot be empty"
+        )
+    
+    task_DB[task_id].update(newTask)
+
+    return task_DB[task_id]
+
+@app.delete("/tasks/{task_id}")
+def deleteTask(task_id:int):
+    if task_id not in task_DB:
+        raise HTTPException(
+            status_code=404,
+            detail="Task not found"
+        )
+    del task_DB[task_id]
+
+    return {"message": "Task deleted successfully"}
